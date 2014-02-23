4d9e51e0900e4457ab2cc9089fc0a6b7f73bb66d
earlhickey <pgroot@gmail.com>
2014-02-23 18:18:03 +0100

    Create read-p1.sql

---
+++ read-p1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE energy(
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   power_usage_low NUMERIC,
+   power_usage_hi NUMERIC,
+   power_return_low NUMERIC,
+   power_return_hi NUMERIC,
+   current_power_usage NUMERIC,
+   current_power_return NUMERIC,
+   gas_usage NUMERIC,
+   datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
